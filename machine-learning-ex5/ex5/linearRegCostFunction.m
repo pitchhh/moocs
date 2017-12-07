5789cac0a185870e37a0c9656a1a646602f016c9
Pichamon Meteveravong <Pichamon.Meteveravong@ibm.com>
2017-12-07 17:59:10 -0600

    Complete ex5

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -1,34 +1,37 @@
 function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
-%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
+%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear
 %regression with multiple variables
-%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
-%   cost of using theta as the parameter for linear regression to fit the 
+%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the
+%   cost of using theta as the parameter for linear regression to fit the
 %   data points in X and y. Returns the cost in J and the gradient in grad
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost and gradient of regularized linear 
+% Instructions: Compute the cost and gradient of regularized linear
 %               regression for a particular choice of theta.
 %
 %               You should set J to the cost and grad to the gradient.
 %
 
+% --- cost function --- %
 
+% calculate hypothesis, or prediction
+h = X * theta;
+% define number of features
+n = size(theta);
+% calculate cost function of regularized linear regression, excluding theta0 term
+J = 1/(2*m)*sum((h-y).^2) + lambda/(2*m)*sum(theta(2:n).^2);
 
+% --- gradient --- %
 
-
-
-
-
-
-
-
+grad = (1/m) * X' * (h-y);  % for all partial derivatives
+grad(2:n) = grad(2:n) + (lambda/m)*theta(2:n);  % add regularized parameter for j>=1 terms
 
 % =========================================================================
 
--- machine-learning-ex5/ex5/validationCurve.m
+++ machine-learning-ex5/ex5/validationCurve.m
@@ -17,36 +17,43 @@ error_train = zeros(length(lambda_vec), 1);
 error_val = zeros(length(lambda_vec), 1);
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return training errors in 
-%               error_train and the validation errors in error_val. The 
-%               vector lambda_vec contains the different lambda parameters 
-%               to use for each calculation of the errors, i.e, 
-%               error_train(i), and error_val(i) should give 
-%               you the errors obtained after training with 
+% Instructions: Fill in this function to return training errors in
+%               error_train and the validation errors in error_val. The
+%               vector lambda_vec contains the different lambda parameters
+%               to use for each calculation of the errors, i.e,
+%               error_train(i), and error_val(i) should give
+%               you the errors obtained after training with
 %               lambda = lambda_vec(i)
 %
 % Note: You can loop over lambda_vec with the following:
 %
 %       for i = 1:length(lambda_vec)
 %           lambda = lambda_vec(i);
-%           % Compute train / val errors when training linear 
+%           % Compute train / val errors when training linear
 %           % regression with regularization parameter lambda
 %           % You should store the result in error_train(i)
 %           % and error_val(i)
 %           ....
-%           
+%
 %       end
 %
 %
 
+for i = 1:length(lambda_vec)
+    lambda = lambda_vec(i);
+    % compute train / val errors
+    % X_subset = X(1:i, :);
+    % y_subset = y(1:i);
 
+    % find theta using trainLinearReg using lambda
+    theta = trainLinearReg(X, y, lambda);
 
-
-
-
-
-
-
+    % do not use regularization when computing errors, hence lambda=0
+    % compute Jtrain
+    error_train(i) = linearRegCostFunction(X, y, theta, 0);
+    % compute Jval
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+end
 
 % =========================================================================
 
